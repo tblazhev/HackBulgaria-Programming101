abd34216e8b7f7c1a169431db03066d2e73649d4
tblazhev <tblazhev@users.noreply.github.com>
2014-03-07 18:42:13 +0200

    Added biggest_difference

---
+++ Week_0/biggest_difference/solution.py
@@ -0,0 +1,13 @@
+def biggest_difference(arr):
+	arr = sorted(arr)
+	return arr[0] - arr[len(arr) - 1]
+
+
+def main():
+	print(biggest_difference([1,2]))
+	print(biggest_difference([1,2,3,4,5]))
+	print(biggest_difference([-10, -9, -1]))
+	print(biggest_difference(range(100)))
+
+if __name__ == '__main__':
+	main()
