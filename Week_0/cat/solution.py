677f19a4c748cd13d07752a0ec5b398980ddaadf
tblazhev <tblazhev@users.noreply.github.com>
2014-03-07 18:51:22 +0200

    Added cat

---
+++ Week_0/cat/solution.py
@@ -0,0 +1,14 @@
+import sys
+
+def main():
+	if len(sys.argv) > 1:
+		filename = sys.argv[1]
+		fp = open(filename, 'r');
+		contents = fp.read()
+		print(contents)
+		fp.close()
+	else:
+		print("No arguments given.")
+
+if __name__ == '__main__':
+	main()
