f425196fe5243d0aa13ed3719f329865e0add6a8
tblazhev <tblazhev@users.noreply.github.com>
2014-03-07 18:48:05 +0200

    Added calculate_coins

---
+++ Week_0/calculate_coins/solution.py
@@ -0,0 +1,23 @@
+coins = [1,2,5,10,20,50,100]
+coins = [100, 50, 20, 10, 5, 2, 1]
+
+
+def calculate_coins(sum):
+	int_sum = sum * 100
+
+	num_coins = {}
+	for coin in coins:
+		count = 0
+		while int_sum >= coin:
+			int_sum -= coin
+			count += 1
+		num_coins[coin] = count
+
+	return num_coins
+
+def main():
+	print(calculate_coins(0.53))
+	print(calculate_coins(8.94))
+
+if __name__ == '__main__':
+	main()
