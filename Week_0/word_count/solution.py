991795dc68ba830631a6656d2bd388315720b692
tblazhev <tblazhev@users.noreply.github.com>
2014-03-07 18:47:43 +0200

    Added word_count

---
+++ Week_0/word_count/solution.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def main():
+	if len(sys.argv) > 1:
+		command = sys.argv[1]
+		filename = sys.argv[2]
+		f = open(filename, 'r');
+		contents = f.read();
+		f.close()
+
+		if command == 'chars':
+			print(len(contents))
+		elif command == 'words':
+			words = contents.split(" ");
+			print(len(words))
+		elif command == 'lines':
+			lines = contents.split("\n")
+			print(len(lines))
+	else:
+		print("No arguments given.")
+
+
+if __name__ == '__main__':
+	main()
