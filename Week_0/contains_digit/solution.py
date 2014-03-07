b170fa7d675f2963ac69b50699ddac77a20c3910
tblazhev <tblazhev@users.noreply.github.com>
2014-03-07 18:43:14 +0200

    Added contains_digit

---
+++ Week_0/contains_digit/solution.py
@@ -0,0 +1,15 @@
+def contains_digit(num, digit):
+	while num != 0:
+		if num % 10 == digit:
+			return True
+		num //= 10
+	return False
+
+def main():
+	print(contains_digit(123, 4))
+	print(contains_digit(42, 2))
+	print(contains_digit(1000, 0))
+	print(contains_digit(12346789, 5))	
+
+if __name__ == '__main__':
+	main()
