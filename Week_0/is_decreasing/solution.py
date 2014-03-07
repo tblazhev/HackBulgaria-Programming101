b0f9156a971ee03a273ab28ba14b70df05bbe82f
tblazhev <tblazhev@users.noreply.github.com>
2014-03-07 18:52:14 +0200

    Added is_decreasing

---
+++ Week_0/is_decreasing/solution.py
@@ -0,0 +1,15 @@
+def is_decreasing(seq):
+	for i in range(0, len(seq) - 1):
+		if seq[i] <= seq[i + 1]:
+			return False
+	return True
+
+
+def main():
+	print(is_decreasing([5,4,3,2,1]))
+	print(is_decreasing([1,2,3]))
+	print(is_decreasing([100, 50, 20]))
+	print(is_decreasing([1,1,1,1]))
+
+if __name__ == '__main__':
+	main()
