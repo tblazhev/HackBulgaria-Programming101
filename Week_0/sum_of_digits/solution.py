8c1729d48a3745b10833f10892676a4e23e1c42f
tblazhev <tblazhev@users.noreply.github.com>
2014-03-07 18:47:11 +0200

    Added sum_of_digits

---
+++ Week_0/sum_of_digits/solution.py
@@ -0,0 +1,13 @@
+def sum_of_digits(num):
+	total = 0
+	while num != 0:
+		total += (num % 10)
+		num = num // 10
+	return total
+
+def main():
+	print(sum_of_digits(1325132435356))
+	print(sum_of_digits(123))
+
+if __name__ == '__main__':
+	main()
