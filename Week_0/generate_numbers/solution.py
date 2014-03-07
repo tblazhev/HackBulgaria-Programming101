34a434fcf12c67876c6f9be132dc9ba70e27c300
tblazhev <tblazhev@users.noreply.github.com>
2014-03-07 18:55:00 +0200

    Added generate_numbers

---
+++ Week_0/generate_numbers/solution.py
@@ -0,0 +1,20 @@
+import sys
+from random import randint
+
+
+def main():
+	if len(sys.argv) > 1:
+		filename = sys.argv[1]
+		n = int(sys.argv[2])
+		f = open(filename, 'w')
+		numbers = []
+		for i in range(0, n):
+			numbers.append(str(randint(1, 1000)))
+		f.write(" ".join(numbers))
+		f.write("\n")
+		f.close()
+	else:
+		print("No arguments given.")
+
+if __name__ == '__main__':
+	main()
