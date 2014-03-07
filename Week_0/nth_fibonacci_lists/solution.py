50f2bde92eedbe94beed2019ee66b9935a5f6df8
tblazhev <tblazhev@users.noreply.github.com>
2014-03-07 18:45:15 +0200

    Added nth_fibonacci_lists

---
+++ Week_0/nth_fibonacci_lists/solution.py
@@ -0,0 +1,30 @@
+def nth_fib_lists(listA, listB, n):
+	if n == 1:
+		return listA
+	elif n == 2:
+		return listB
+
+	a = listA
+	b = listB
+	index = 2
+
+	while index < n:
+		next = b + a
+		a = b
+		b = next
+		index += 1
+
+	return b
+
+
+def main():
+	print(nth_fib_lists([1,2], [3,4], 4))
+
+	print(nth_fib_lists([1], [2], 1))
+	print(nth_fib_lists([1], [2], 2))
+	print(nth_fib_lists([1, 2], [1, 3], 3))
+	print(nth_fib_lists([], [1, 2, 3], 4))
+	print(nth_fib_lists([], [], 100))
+
+if __name__ == '__main__':
+	main()
