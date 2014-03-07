87bd017d4f06cdaafc6e15ad0a3a0914d5a35115
tblazhev <tblazhev@users.noreply.github.com>
2014-03-07 18:49:09 +0200

    Added is_an_bn

---
+++ Week_0/is_an_bn/solution.py
@@ -0,0 +1,21 @@
+def is_an_bn(word):
+	n = len(word)
+	if n % 2 != 0:
+		return False
+	n = n // 2
+	check = 'a' * n + 'b' * n
+	if word == check:
+		return True
+	return False
+
+def main():
+	print(is_an_bn(""))
+	print(is_an_bn("rado"))
+	print(is_an_bn("aaabb"))
+	print(is_an_bn("aaabbb"))
+	print(is_an_bn("aabbaabb"))
+	print(is_an_bn("bbbaaa"))
+	print(is_an_bn("aaaaabbbbb"))
+
+if __name__ == '__main__':
+	main()
