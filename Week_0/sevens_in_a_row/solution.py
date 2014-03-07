ecc4bcaead09c6c602678fda98210a4657dcaabd
tblazhev <tblazhev@users.noreply.github.com>
2014-03-07 18:40:53 +0200

    Added sevens_in_a_row

---
+++ Week_0/sevens_in_a_row/solution.py
@@ -0,0 +1,19 @@
+def sevens_in_a_row(arr, n):
+	count_sevens = 0
+	for item in arr:
+		if item == 7:
+			count_sevens += 1
+		else:
+			count_sevens = 0
+		if count_sevens == n:
+			return True
+	return False
+
+def main():
+	print(sevens_in_a_row([10,8,7,6,7,7,7,20,-7], 3))
+	print(sevens_in_a_row([1,7,1,7,7], 4))
+	print(sevens_in_a_row([7,7,7,1,1,1,7,7,7,7], 3))
+	print(sevens_in_a_row([7,2,1,6,2], 1))
+
+if __name__ == '__main__':
+	main()
