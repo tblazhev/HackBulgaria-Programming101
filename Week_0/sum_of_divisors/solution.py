4fd14cd19639ec6013f31c9bf0b4364119418ce7
tblazhev <tblazhev@users.noreply.github.com>
2014-03-07 18:50:45 +0200

    Added sum_of_divisors

---
+++ Week_0/sum_of_divisors/solution.py
@@ -0,0 +1,15 @@
+from math import sqrt
+
+def sum_of_divisors(num):
+	total = 0
+	for i in range(1, num + 1):
+		if num % i == 0:
+			total += i
+	return total
+
+def main():
+	print(sum_of_divisors(7))
+	print(sum_of_divisors(1000))
+
+if __name__ == '__main__':
+	main()
