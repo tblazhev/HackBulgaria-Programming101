5fd197216a1f03d53e5a3fd2f2ca04ed6e55fe07
tblazhev <tblazhev@users.noreply.github.com>
2014-03-07 18:43:32 +0200

    Added contains_digits

---
+++ Week_0/contains_digits/solution.py
@@ -0,0 +1,24 @@
+def contains_digit(num, digit):
+	while num != 0:
+		if num % 10 == digit:
+			return True
+		num //= 10
+	return False
+
+def contains_digits(num, digits):
+	if len(digits) == 0:
+		return True
+	for digit in digits:
+		if not contains_digit(num, digit):
+			return False
+	return True
+
+
+def main():
+	print(contains_digits(402123, [0, 3, 4]))
+	print(contains_digits(666, [6,4]))
+	print(contains_digits(123456789, [1,2,3,0]))
+	print(contains_digits(456, []))
+
+if __name__ == '__main__':
+	main()
