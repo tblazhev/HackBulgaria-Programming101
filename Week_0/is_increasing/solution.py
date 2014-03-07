b482b9a44e73dce0130f72716d2deaf0834a7c5a
tblazhev <tblazhev@users.noreply.github.com>
2014-03-07 18:56:03 +0200

    Added is_increasing

---
+++ Week_0/is_increasing/solution.py
@@ -0,0 +1,15 @@
+def is_increasing(seq):
+	for i in range(0, len(seq) - 1):
+		if seq[i] >= seq[i + 1]:
+			return False
+	return True
+
+
+def main():
+	print(is_increasing([1,2,3,4,5]))
+	print(is_increasing([1]))
+	print(is_increasing([5,6,-10]))
+	print(is_increasing([1,1,1,1]))
+
+if __name__ == '__main__':
+	main()
