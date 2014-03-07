3cb211c7cfdeca7374c056c266f85e79945d660e
tblazhev <tblazhev@users.noreply.github.com>
2014-03-07 18:51:56 +0200

    Added fibonacci

---
+++ Week_0/fibonacci/solution.py
@@ -0,0 +1,19 @@
+def nth_fibonacci(n):
+	a = 1
+	b = 1
+	index = 2
+
+	while index < n:
+		next = a + b
+		a = b
+		b = next
+		index = index + 1
+		
+	return b
+
+def main():
+	print(nth_fibonacci(100))
+	print(nth_fibonacci(2))
+
+if __name__ == '__main__':
+	main()
