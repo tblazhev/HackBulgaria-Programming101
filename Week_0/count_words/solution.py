aef2688fa6745da8b43ce2cf6ea732ab607b1b54
tblazhev <tblazhev@users.noreply.github.com>
2014-03-07 18:44:06 +0200

    Added count_words

---
+++ Week_0/count_words/solution.py
@@ -0,0 +1,17 @@
+def count_words(arr):
+	word_count = {}
+
+	for word in arr:
+		if word not in word_count:
+			word_count[word] = 0
+		word_count[word] += 1
+	return word_count
+
+
+def main():
+	print(count_words(["apple", "banana", "apple", "pie"]))
+	print(count_words(["python", "python", "python", "ruby"]))
+
+
+if __name__ == '__main__':
+	main()
