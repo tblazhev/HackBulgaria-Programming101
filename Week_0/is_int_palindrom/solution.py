7fec61e4824a375144593b974c25ce9cb4103250
tblazhev <tblazhev@users.noreply.github.com>
2014-03-07 18:39:17 +0200

    Added is_int_palindrom

---
+++ Week_0/is_int_palindrom/solution.py
@@ -0,0 +1,14 @@
+def is_int_palindrom(num):
+	if str(num) == str(num)[::-1]:
+		return True
+	return False
+
+def main():
+	print(is_int_palindrom(1))
+	print(is_int_palindrom(42))
+	print(is_int_palindrom(100001))
+	print(is_int_palindrom(999))
+	print(is_int_palindrom(123))
+
+if __name__ == '__main__':
+	main()
