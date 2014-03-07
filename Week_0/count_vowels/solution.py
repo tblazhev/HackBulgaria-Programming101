9c4129a2a7ab4ccbc2a8a80fbb8b52172208214a
tblazhev <tblazhev@users.noreply.github.com>
2014-03-07 18:37:55 +0200

    Added count_vowels

---
+++ Week_0/count_vowels/solution.py
@@ -0,0 +1,19 @@
+def count_vowels(string):
+	count = 0
+	vowels = 'aeiouy'
+	for s in string:
+		if s.lower() in vowels:
+			count += 1
+	return count
+
+
+def main():
+	print(count_vowels("Python"))
+	print(count_vowels("Theistareykjarbunga")) #It's a volcano name!
+	print(count_vowels("grrrrgh!"))
+	print(count_vowels("Github is the second best thing that happend to programmers, after the keyboard!"))
+	print(count_vowels("A nice day to code!"))
+
+
+if __name__ == '__main__':
+	main()
