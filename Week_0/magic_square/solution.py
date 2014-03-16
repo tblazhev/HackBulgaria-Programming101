e5987377419b82dfb5b67e32578176decaa5408b
tblazhev <tblazhev@users.noreply.github.com>
2014-03-16 11:21:54 +0200

    Added tests for magic_square

--- Week_0/magic_square/solution.py
+++ Week_0/magic_square/solution.py
@@ -1,12 +1,12 @@
 def magic_square(matrix):
     rows_sum = sum(matrix[0])
     len_matrix = len(matrix)
-    
+
     for row in matrix:
         current_row_sum = sum(row)
         if current_row_sum != rows_sum:
             return False
-    
+
     col_sums = {}
     last_col_sum = 0
     for i in range(0, len_matrix):
@@ -18,7 +18,7 @@ def magic_square(matrix):
         if i != 0 and last_col_sum != col_sums[i]:
             return False
         last_col_sum = col_sums[i]
-    
+
     left_diagonal = 0
     right_diagonal = 0
     for i in range(0, len_matrix):
@@ -30,9 +30,9 @@ def magic_square(matrix):
 
 
 def main():
-    print(magic_square([[1,2,3], [4,5,6], [7,8,9]]))
-    print(magic_square([[4,9,2], [3,5,7], [8,1,6]]))
-    print(magic_square([[7,12,1,14], [2,13,8,11], [16,3,10,5], [9,6,15,4]]))
+    print(magic_square([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+    print(magic_square([[4, 9, 2], [3, 5, 7], [8, 1, 6]]))
+    print(magic_square([[7, 12, 1, 14], [2, 13, 8, 11], [16, 3, 10, 5], [9, 6, 15, 4]]))
     print(magic_square([[23, 28, 21], [22, 24, 26], [27, 20, 25]]))
     print(magic_square([[16, 23, 17], [78, 32, 21], [17, 16, 15]]))
 
