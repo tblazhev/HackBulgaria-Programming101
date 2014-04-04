730fed4eba66f198b9f4fd9f642cbee9da4823fa
tblazhev <tblazhev@users.noreply.github.com>
2014-04-04 13:43:57 +0300

    Added initial tests for animal builder

---
+++ Week5/zoo/animal_builder.py
@@ -0,0 +1,3 @@
+class AnimalBuilder():
+    def __init__(self):
+        pass
---
+++ Week5/zoo/animal_builder_tests.py
@@ -0,0 +1,18 @@
+import unittest
+from animal_builder import AnimalBuilder
+
+
+class AnimalBuilderTests(unittest.TestCase):
+    """docstring for AnimalBuilderTest"""
+    def setUp(self):
+        self.animal_builder = AnimalBuilder()
+
+    def test_new(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
